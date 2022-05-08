6accb43343d5b80ea7e8e67bab52e1925f0acdce
knmn2000 <karan1501mannan@gmail.com>
2022-05-08 12:06:41 +0530

    init commit. add recording logic

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {
+  Dimensions,
+  Platform,
+  PermissionsAndroid,
+  SafeAreaView,
+  StyleSheet,
+  Button,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
+
+const App = () => {
+  return <HomeScreen />;
+};
+export default App;
---
+++ src/screens/RecorderScreen.js
@@ -0,0 +1,359 @@
+import React, {useEffect, useState, useMemo} from 'react';
+import AudioRecorderPlayer, {
+  AVEncoderAudioQualityIOSType,
+  AVEncodingOption,
+  AudioEncoderAndroidType,
+  AudioSourceAndroidType,
+} from 'react-native-audio-recorder-player';
+import {
+  Dimensions,
+  Platform,
+  PermissionsAndroid,
+  SafeAreaView,
+  StyleSheet,
+  Button,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import RNFetchBlob from 'rn-fetch-blob';
+
+const App = () => {
+  const screenWidth = Dimensions.get('screen').width;
+  const dirs = RNFetchBlob.fs.dirs;
+  const path = Platform.select({
+    ios: 'hello.m4a',
+    android: `${dirs.MainBundleDir}/hello.mp3`,
+  });
+  const [audioRecorderPlayer, setAudioRecorderPlayer] = useState();
+  useMemo(() => {
+    setAudioRecorderPlayer(new AudioRecorderPlayer());
+  }, []);
+  const [playerState, setPlayerState] = useState({
+    isLoggingIn: false,
+    recordSecs: 0,
+    recordTime: '00:00:00',
+    currentPositionSec: 0,
+    currentDurationSec: 0,
+    playTime: '00:00:00',
+    duration: '00:00:00',
+  });
+  useEffect(() => {
+    audioRecorderPlayer.setSubscriptionDuration(0.1);
+  }, [audioRecorderPlayer]);
+  let playWidth =
+    (playerState.currentPositionSec / playerState.currentDurationSec) *
+    (screenWidth - 56);
+
+  if (!playWidth) {
+    playWidth = 0;
+  }
+  const onStatusPress = e => {
+    const touchX = e.nativeEvent.locationX;
+    console.log(`touchX: ${touchX}`);
+    playWidth =
+      (playerState.currentPositionSec / playerState.currentDurationSec) *
+      (screenWidth - 56);
+    console.log(`currentPlayWidth: ${playWidth}`);
+
+    const currentPosition = Math.round(playerState.currentPositionSec);
+
+    if (playWidth && playWidth < touchX) {
+      const addSecs = Math.round(currentPosition + 1000);
+      audioRecorderPlayer.seekToPlayer(addSecs);
+      console.log(`addSecs: ${addSecs}`);
+    } else {
+      const subSecs = Math.round(currentPosition - 1000);
+      audioRecorderPlayer.seekToPlayer(subSecs);
+      console.log(`subSecs: ${subSecs}`);
+    }
+  };
+
+  const onStartRecord = async () => {
+    if (Platform.OS === 'android') {
+      try {
+        const grants = await PermissionsAndroid.requestMultiple([
+          PermissionsAndroid.PERMISSIONS.WRITE_EXTERNAL_STORAGE,
+          PermissionsAndroid.PERMISSIONS.READ_EXTERNAL_STORAGE,
+          PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+        ]);
+
+        console.log('write external stroage', grants);
+
+        if (
+          grants['android.permission.WRITE_EXTERNAL_STORAGE'] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          grants['android.permission.READ_EXTERNAL_STORAGE'] ===
+            PermissionsAndroid.RESULTS.GRANTED &&
+          grants['android.permission.RECORD_AUDIO'] ===
+            PermissionsAndroid.RESULTS.GRANTED
+        ) {
+          console.log('permissions granted');
+        } else {
+          console.log('All required permissions not granted');
+          return;
+        }
+      } catch (err) {
+        console.warn(err);
+        return;
+      }
+    }
+
+    const audioSet = {
+      AudioEncoderAndroid: AudioEncoderAndroidType.AAC,
+      AudioSourceAndroid: AudioSourceAndroidType.MIC,
+      AVEncoderAudioQualityKeyIOS: AVEncoderAudioQualityIOSType.high,
+      AVNumberOfChannelsKeyIOS: 2,
+      AVFormatIDKeyIOS: AVEncodingOption.aac,
+    };
+
+    console.log('audioSet', audioSet);
+    //? Custom path
+    // const uri = await audioRecorderPlayer.startRecorder(
+    //   path,
+    //   audioSet,
+    // );
+
+    //? Default path
+    const uri = await audioRecorderPlayer.startRecorder(path.android, audioSet);
+
+    audioRecorderPlayer.addRecordBackListener(e => {
+      console.log('record-back', e);
+      setPlayerState({
+        ...playerState,
+        recordSecs: e.currentPosition,
+        recordTime: audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)),
+      });
+    });
+    console.log(`uri: ${uri}`);
+  };
+
+  const onPauseRecord = async () => {
+    try {
+      await audioRecorderPlayer.pauseRecorder();
+    } catch (err) {
+      console.log('pauseRecord', err);
+    }
+  };
+
+  const onResumeRecord = async () => {
+    await audioRecorderPlayer.resumeRecorder();
+  };
+
+  const onStopRecord = async () => {
+    const result = await audioRecorderPlayer.stopRecorder();
+    audioRecorderPlayer.removeRecordBackListener();
+    setPlayerState({...playerState, recordSecs: 0});
+    console.log(result);
+  };
+
+  const onStartPlay = async () => {
+    console.log('onStartPlay');
+    //? Custom path
+    // const msg = await audioRecorderPlayer.startPlayer(path);
+
+    //? Default path
+    const msg = await audioRecorderPlayer.startPlayer(path.android);
+    const volume = await audioRecorderPlayer.setVolume(1.0);
+    console.log(`file: ${msg}`, `volume: ${volume}`);
+
+    audioRecorderPlayer.addPlayBackListener(e => {
+      setPlayerState({
+        ...playerState,
+        currentPositionSec: e.currentPosition,
+        currentDurationSec: e.duration,
+        playTime: audioRecorderPlayer.mmssss(Math.floor(e.currentPosition)),
+        duration: audioRecorderPlayer.mmssss(Math.floor(e.duration)),
+      });
+    });
+  };
+
+  const onPausePlay = async () => {
+    await audioRecorderPlayer.pausePlayer();
+  };
+
+  const onResumePlay = async () => {
+    await audioRecorderPlayer.resumePlayer();
+  };
+
+  const onStopPlay = async () => {
+    console.log('onStopPlay');
+    audioRecorderPlayer.stopPlayer();
+    audioRecorderPlayer.removePlayBackListener();
+  };
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titleTxt}>Audio Recorder Player</Text>
+      <Text style={styles.txtRecordCounter}>{playerState.recordTime}</Text>
+      <View style={styles.viewRecorder}>
+        <View style={styles.recordBtnWrapper}>
+          <Button
+            style={styles.btn}
+            onPress={onStartRecord}
+            title="record"
+            textStyle={styles.txt}
+          />
+          <Button
+            style={[
+              styles.btn,
+              {
+                marginLeft: 12,
+              },
+            ]}
+            onPress={onPauseRecord}
+            title="pause"
+            textStyle={styles.txt}
+          />
+          <Button
+            title="resume"
+            style={[
+              styles.btn,
+              {
+                marginLeft: 12,
+              },
+            ]}
+            onPress={onResumeRecord}
+            textStyle={styles.txt}
+          />
+          <Button
+            style={[styles.btn, {marginLeft: 12}]}
+            onPress={onStopRecord}
+            title="stop"
+            textStyle={styles.txt}
+          />
+        </View>
+      </View>
+      <View style={styles.viewPlayer}>
+        <TouchableOpacity style={styles.viewBarWrapper} onPress={onStatusPress}>
+          <View style={styles.viewBar}>
+            <View style={[styles.viewBarPlay, {width: playWidth}]} />
+          </View>
+        </TouchableOpacity>
+        <Text style={styles.txtCounter}>
+          {playerState.playTime} / {playerState.duration}
+        </Text>
+        <View style={styles.playBtnWrapper}>
+          <Button
+            style={styles.btn}
+            onPress={onStartPlay}
+            title="play"
+            textStyle={styles.txt}
+          />
+          <Button
+            style={[
+              styles.btn,
+              {
+                marginLeft: 12,
+              },
+            ]}
+            onPress={onPausePlay}
+            title="pause"
+            textStyle={styles.txt}
+          />
+          <Button
+            style={[
+              styles.btn,
+              {
+                marginLeft: 12,
+              },
+            ]}
+            onPress={onResumePlay}
+            title="resume"
+            textStyle={styles.txt}
+          />
+          <Button
+            style={[
+              styles.btn,
+              {
+                marginLeft: 12,
+              },
+            ]}
+            onPress={onStopPlay}
+            title="stop"
+            textStyle={styles.txt}
+          />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#455A64',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  titleTxt: {
+    marginTop: 100,
+    color: 'white',
+    fontSize: 28,
+  },
+  viewRecorder: {
+    marginTop: 40,
+    width: '100%',
+    alignItems: 'center',
+  },
+  recordBtnWrapper: {
+    flexDirection: 'row',
+  },
+  viewPlayer: {
+    marginTop: 60,
+    alignSelf: 'stretch',
+    alignItems: 'center',
+  },
+  viewBarWrapper: {
+    marginTop: 28,
+    marginHorizontal: 28,
+    alignSelf: 'stretch',
+  },
+  viewBar: {
+    backgroundColor: '#ccc',
+    height: 4,
+    alignSelf: 'stretch',
+  },
+  viewBarPlay: {
+    backgroundColor: 'white',
+    height: 4,
+    width: 0,
+  },
+  playStatusTxt: {
+    marginTop: 8,
+    color: '#ccc',
+  },
+  playBtnWrapper: {
+    flexDirection: 'row',
+    marginTop: 40,
+  },
+  btn: {
+    borderColor: 'white',
+    borderWidth: 1,
+  },
+  txt: {
+    color: 'white',
+    fontSize: 14,
+    marginHorizontal: 8,
+    marginVertical: 4,
+  },
+  txtRecordCounter: {
+    marginTop: 32,
+    color: 'white',
+    fontSize: 20,
+    textAlignVertical: 'center',
+    fontWeight: '200',
+    fontFamily: 'Helvetica Neue',
+    letterSpacing: 3,
+  },
+  txtCounter: {
+    marginTop: 12,
+    color: 'white',
+    fontSize: 20,
+    textAlignVertical: 'center',
+    fontWeight: '200',
+    fontFamily: 'Helvetica Neue',
+    letterSpacing: 3,
+  },
+});
+
+export default App;
